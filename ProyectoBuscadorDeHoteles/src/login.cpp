c68831e6c638678c331cee66569d4798e8f93f62
ikergallar <ikergallar@opendeusto.es>
2020-05-19 11:58:34 +0200

    iniciarSesion

--- ProyectoBuscadorDeHoteles/src/login.cpp
+++ ProyectoBuscadorDeHoteles/src/login.cpp
@@ -1,67 +1,92 @@
 #include "login.h"
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <cstdlib>
+#include <conio.h> // getch
+#include <vector>
+
+#define ENTER 13
+#define BACKSPACE 8
+
 using namespace std;
 
-login::login(char *n,char *p)
+void login:: iniciarSesion()
 {
-    nombre = new char[strlen(n)+1];
-    strcpy(nombre,n);
-    pass = new char[strlen(n)+1];
-    strcpy(pass,p);
+    vector<string> usuarios;
+    vector<string> claves;
 
-}
-void login::iniciarSesion(){
+    // Se añaden usuarios al vector
+    usuarios.push_back("luis");
+    usuarios.push_back("jorge");
+    usuarios.push_back("james");
+    usuarios.push_back("johan");
+    usuarios.push_back("kevin");
 
-    int i;
-    for(i=1; i<4; i++){
+    // Se añaden claves al vector
+    claves.push_back("123");
+    claves.push_back("456");
+    claves.push_back("789");
+    claves.push_back("abc");
+    claves.push_back("efg");
+
+
+    string usuario, password;
+
+    int contador = 0;
+    bool ingresa = false;
+
+    do {
         system("cls");
-        cout << endl;
-        cout << "        BIENVENIDO AL SISTEMA DE LOGIN " << endl;
-        cout << "___" << endl;
-        cout << endl;
-        cout << "Por favor ingrese su informacion de usuario" << endl;
-        cout << endl;
-        cout << "¿Nombre de usuario?" << endl;
-        cin >> nombre;
-    if(nombre == " iker"){
-        cout << "Nombre de usuario correcto, por favor ingrese su contrasena:" << endl;
-        cin >> pass;
-    if(pass == "admin123"){
-        cout << endl;
-        cout << "Contrasena correcta, bienvenido, sera redirigido a la aplicacion" << endl;
-        system("pause>null");
-        exit(0);
-    }
-    else
-        if(pass != "admin123"){
-            cout << "Contrasena incorrecta, por favor ingrese un usuario y contrasena valida" << endl;
-            system("pause>null");
+        cout << "\t\t\tLOGIN DE USUARIO" << endl;
+        cout << "\t\t\t----------------" << endl;
+        cout << "\n\tUsuario: ";
+        getline(cin, usuario);
+
+        char caracter;
+
+        cout << "\tPassword: ";
+        caracter = getch();
+
+        password = "";
+
+        while (caracter != ENTER) {
+
+            if (caracter != BACKSPACE) {
+                password.push_back(caracter);
+                cout << "*";
+
+            } else {
+                if (password.length() > 0) {
+                    cout << "\b \b";
+                    password = password.substr(0, password.length() - 1);
+                }
+            }
+
+            caracter = getch();
         }
-    }
-    else
-        if(nombre != "admin"){
-            cout << "Nombre de usuario incorrecto, por favor ingrese su nombre de usuario nuevamente" << endl;
-            system("pause>null");
+
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios[i] == usuario && claves[i] == password) {
+                ingresa = true;
+                break;
+            }
         }
 
-    if(i > 0){
-        cout << endl;
-        cout << "Ha realizado " << i << "/3 intentos" << endl;
-        cout << endl;
-        system("pause>null");
-   }
-    if(i == 3){
-        cout << endl;
-        cout << "Ha realizado 3 intentos, por seguridad el programa se cerrara..." << endl;
-        system("pause>null");
-        exit(0);
-    }
+        if (!ingresa) {
+            cout << "\n\n\tEl usuario y/o password son incorrectos" << endl;
+            cin.get();
+            contador++;
+        }
+
+    } while (ingresa == false && contador < 3);
+
+    if (ingresa == false) {
+        cout << "\n\tUsted no pudo ingresar al sistema. ADIOS" << endl;
+    } else {
+        cout << "\n\n\tBienvenido al sistema" << endl;
     }
 
-}
+    cin.get();
 
-login::~login()
-{
-    delete[] nombre, pass;
+
 }
